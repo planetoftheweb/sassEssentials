3877f99ee60c3dcb51ddf5c47e39b543b336a504
Ray Villalobos <ray@planetoftheweb.com>
2019-06-05 17:37:18 -0400

    Updated to new gulp process using node-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,54 @@
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    sass = require('gulp-ruby-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    webserver = require('gulp-webserver');
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  sourcemaps = require("gulp-sourcemaps"),
+  browserSync = require("browser-sync").create(),
+  source = "./process/",
+  dest = "./builds/sassEssentials/";
 
-gulp.task('js', function() {
-  return gulp.src('builds/sassEssentials/js/myscript.js')
-    .pipe(jshint('./.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
-});
+sass.compiler = require("node-sass");
 
-gulp.task('sass', function () {
-    return sass('process/sass/style.scss', {
-      sourcemap: true,
-      style: 'expanded'
-    })
-    .on('error', function (err) {
-        console.error('Error!', err.message);
-    })
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('builds/sassEssentials/css'));
-});
+function html() {
+  return gulp.src(dest + "**/*.html");
+}
 
-gulp.task('watch', function() {
-  gulp.watch('builds/sassEssentials/js/**/*', ['js']);
-  gulp.watch(['process/sass/**/*'], ['sass']);
-});
+function js() {
+  return gulp.src(dest + "**/*.js");
+}
 
-gulp.task('webserver', function() {
-    gulp.src('builds/sassEssentials/')
-        .pipe(webserver({
-            livereload: true,
-            open: true
-        }));
-});
+function styles() {
+  return gulp
+    .src(source + "sass/style.scss")
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        sourcemap: true,
+        style: "compressed"
+      }).on("error", sass.logError)
+    )
+    .pipe(gulp.dest(dest + "css"));
+}
 
-gulp.task('default', ['watch', 'sass','webserver']);
+function watch() {
+  gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+  gulp.watch(source + "sass/**/*", styles).on("change", browserSync.reload);
+  gulp.watch(dest + "index.html", html).on("change", browserSync.reload);
+}
+
+function server() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: dest
+    }
+  });
+
+  gulp
+    .watch(source + "sass/**/*.scss", styles)
+    .on("change", browserSync.reload);
+  gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+  gulp.watch(dest + "index.html", html).on("change", browserSync.reload);
+}
+
+var build = gulp.series(gulp.parallel(js, styles, html), server, watch);
+
+gulp.task("default", build);
