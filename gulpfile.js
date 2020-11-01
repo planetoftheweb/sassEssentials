e93b4cfd1c7ed2be10aa44dbb308b31a14ee88ba
JamesVanWaza <jgacuca@gmail.com>
2020-11-01 17:37:54 -0500

    Using the current version from master branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,20 @@
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    sass = require("gulp-sass"),
-    sourcemaps = require('gulp-sourcemaps'),
-    webserver = require('gulp-webserver');
-
-gulp.task('js', function() {
-  return gulp.src('builds/sassEssentials/js/myscript.js')
-    .pipe(jshint('./.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('sass', function () {
-    return sass('process/sass/style.scss', {
-      sourcemap: true,
-      style: 'expanded'
-    })
-    .on('error', function (err) {
-        console.error('Error!', err.message);
-    })
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('builds/sassEssentials/css'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('builds/sassEssentials/js/**/*', ['js']);
-  gulp.watch(['process/sass/**/*'], ['sass']);
-});
-
-gulp.task('webserver', function() {
-    gulp.src('builds/sassEssentials/')
-        .pipe(webserver({
-            livereload: true,
-            open: true
-        }));
-});
-
-gulp.task('default', ['watch', 'sass','webserver']);
+{
+  "name": "sassessentials",
+  "version": "1.0.2",
+  "description": "A website for my Sass Essential Training course on Lynda.com",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/planetoftheweb/sassEssentials.git"
+  },
+  "author": "Ray Villalobos",
+  "devDependencies": {
+    "gulp": "^4.0.2",
+    "gulp-jshint": "^2.1.0",
+    "gulp-sass": "^4.0.2",
+    "gulp-sourcemaps": "^2.6.5",
+    "node-sass": "^4.12.0"
+  },
+  "dependencies": {
+    "browser-sync": "^2.26.5"
+  }
+}
