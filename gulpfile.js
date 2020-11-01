a66e54f9db787e86f5b3ec48dfc0b4c0ac7d6240
JamesVanWaza <jgacuca@gmail.com>
2020-11-01 17:39:17 -0500

    Using master version since it works

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,54 @@
-{
-  "name": "sassessentials",
-  "version": "1.0.2",
-  "description": "A website for my Sass Essential Training course on Lynda.com",
-  "repository": {
-    "type": "git",
-    "url": "https://github.com/planetoftheweb/sassEssentials.git"
-  },
-  "author": "Ray Villalobos",
-  "devDependencies": {
-    "gulp": "^4.0.2",
-    "gulp-jshint": "^2.1.0",
-    "gulp-sass": "^4.0.2",
-    "gulp-sourcemaps": "^2.6.5",
-    "node-sass": "^4.12.0"
-  },
-  "dependencies": {
-    "browser-sync": "^2.26.5"
-  }
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  sourcemaps = require("gulp-sourcemaps"),
+  browserSync = require("browser-sync").create(),
+  source = "./process/",
+  dest = "./builds/sassEssentials/";
+
+sass.compiler = require("node-sass");
+
+function html() {
+  return gulp.src(dest + "**/*.html");
 }
+
+function js() {
+  return gulp.src(dest + "**/*.js");
+}
+
+function styles() {
+  return gulp
+    .src(source + "sass/style.scss")
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        sourcemap: true,
+        style: "compressed"
+      }).on("error", sass.logError)
+    )
+    .pipe(gulp.dest(dest + "css"));
+}
+
+function watch() {
+  gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+  gulp.watch(source + "sass/**/*", styles).on("change", browserSync.reload);
+  gulp.watch(dest + "index.html", html).on("change", browserSync.reload);
+}
+
+function server() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: dest
+    }
+  });
+
+  gulp
+    .watch(source + "sass/**/*.scss", styles)
+    .on("change", browserSync.reload);
+  gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+  gulp.watch(dest + "index.html", html).on("change", browserSync.reload);
+}
+
+var build = gulp.series(gulp.parallel(js, styles, html), server, watch);
+
+gulp.task("default", build);
